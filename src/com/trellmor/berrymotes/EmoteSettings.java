a095ced005e808dcdc2801f5b8e84bc278a560fe
Daniel Triendl <daniel@pew.cc>
2013-08-18 17:57:57 +0200

    Added Settingshelpers

---
+++ src/com/trellmor/berrymotes/EmoteSettings.java
@@ -0,0 +1,102 @@
+/*
+ * LibBerryMotes
+ * 
+ * Copyright (C) 2013 Daniel Triendl <trellmor@trellmor.com>
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package com.trellmor.berrymotes;
+
+import android.content.SharedPreferences;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceManager;
+
+public class EmoteSettings {
+	public static final String KEY_BERRYMOTES_ENABLED = "berrymotes.enabled";
+	public static final String KEY_BERRYMOTES_SETTINGS = "berrymotes.settings";
+
+	private EmoteSettings() {
+	}
+
+	@SuppressWarnings("deprecation")
+	public static void addEmoteSettings(PreferenceActivity context) {
+		// Add 'BerryMotes' preferences, and a corresponding header.
+		PreferenceCategory fakeHeader = new PreferenceCategory(context);
+		fakeHeader.setTitle(R.string.pref_header_berrymotes);
+		context.getPreferenceScreen().addPreference(fakeHeader);
+		context.addPreferencesFromResource(R.xml.pref_emotes);
+
+		SharedPreferences settings = PreferenceManager
+				.getDefaultSharedPreferences(context);
+
+		Preference prefEnabled = context.findPreference(KEY_BERRYMOTES_ENABLED);
+		prefEnabled.setOnPreferenceChangeListener(sEnabledChangeListener);
+		boolean enabled = settings.getBoolean(KEY_BERRYMOTES_ENABLED, false);
+		boolean canEnable = sEnabledChangeListener.onPreferenceChange(prefEnabled,
+				settings.getBoolean(KEY_BERRYMOTES_ENABLED, false));
+		
+		if (enabled && !canEnable) {
+			settings.edit().putBoolean(KEY_BERRYMOTES_ENABLED, false).commit();
+		}
+		
+		Preference prefSettings = context.findPreference(KEY_BERRYMOTES_SETTINGS);
+		prefSettings.setOnPreferenceClickListener(sSettingsClickListener);
+	}
+
+	public static Preference.OnPreferenceChangeListener sEnabledChangeListener = new Preference.OnPreferenceChangeListener() {
+
+		@Override
+		public boolean onPreferenceChange(Preference preference, Object newValue) {
+			boolean canChange = true;
+			if (KEY_BERRYMOTES_ENABLED.equals(preference.getKey())) {
+				boolean boolValue = Boolean.parseBoolean(newValue.toString());
+
+				if (boolValue) {
+					canChange = EmoteUtils.isBerryMotesInstalled(preference
+							.getContext());
+					
+					if (!canChange) {
+						EmoteUtils.showInstallDialog(preference.getContext());
+					}
+				}
+
+				if (boolValue && canChange) {
+					preference
+							.setSummary(R.string.pref_description_berrymotes_enable_true);
+				} else {
+					preference
+							.setSummary(R.string.pref_description_berrymotes_enable_false);
+				}
+			}
+			return canChange;
+		}
+	};
+	
+	public static Preference.OnPreferenceClickListener sSettingsClickListener = new Preference.OnPreferenceClickListener() {
+		
+		@Override
+		public boolean onPreferenceClick(Preference preference) {
+			if (EmoteUtils.isBerryMotesInstalled(preference.getContext())) {
+				EmoteUtils.launchBerryMotesSettings(preference.getContext());
+			} else {
+				EmoteUtils.showInstallDialog(preference.getContext());
+			}
+			return true;
+		}
+	};
+}
--- src/com/trellmor/berrymotes/EmotesFormatter.java
+++ src/com/trellmor/berrymotes/EmotesFormatter.java
@@ -20,9 +20,42 @@
 
 package com.trellmor.berrymotes;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
 public class EmotesFormatter {
-	public static String formatString(String s) {
+	private boolean mEmotesEnabled = false; 
+	private SettingsChangedListener mSettingsChangedListener = new SettingsChangedListener();
+	
+	public EmotesFormatter(Context context) {
+		SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+		mEmotesEnabled = settings.getBoolean(EmoteSettings.KEY_BERRYMOTES_ENABLED, false);
+		settings.registerOnSharedPreferenceChangeListener(mSettingsChangedListener);
+	}
+	
+	public String formatString(String s) {
+		if (mEmotesEnabled) {
+			return replaceEmotes(s);
+		} else {
+			return s;
+		}
+	}
+	
+	public static String replaceEmotes(String s) {
 		return s.replaceAll("\\[\\]\\(\\/([\\w:!#\\/]+)([-\\w!]*)([^)]*)\\)",
 				"<img src=\"$1\" alt=\"$1\" />");
+	}
+	
+	private class SettingsChangedListener implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+		@Override
+		public void onSharedPreferenceChanged(
+				SharedPreferences sharedPreferences, String key) {
+			if (EmoteSettings.KEY_BERRYMOTES_ENABLED.equals(key)) {
+				mEmotesEnabled = sharedPreferences.getBoolean(key, false);
+			}
+		}
+		
 	}
 }
